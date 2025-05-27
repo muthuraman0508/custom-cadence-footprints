9c0a85a2e439002f4b3def0aa518192485a279ce
MUTHU RAMAN S <127648597+muthuraman0508@users.noreply.github.com>
2025-05-27 18:32:19 +0530

    0402_RES_SMD_FootPrint

---
+++ Footprint/Resistor/SMD/0402/dimension.v
@@ -0,0 +1,30 @@
+Resistor: 0402
+Type : SMD
+Units : mm
+
+Pad dimension:
+Shape: Rectangular
+Pad:
+	-> Length => 0.6 mm
+	-> Width => 0.5 mm
+Solder Pad:
+	-> Length => 0.6 mm
+	-> Width => 0.5 mm
+Solder Mask:
+	-> Length => 0.6 + 0.1524 = 0.7524 mm
+	-> Width => 0.5 + 0.1524 = 0.0.6524 mm
+
+
+Pitch : 0.5 + 0.5 => 1.0 mm
+Assemely Top:
+	-> length => 1.0 mm
+	-> Width => 0.5 mm
+	-> Line width => 0.1270 mm
+Place Bound Top:
+	=> height => 0.35 mm
+
+Sill Screen:
+	-> length => 1.0 + 0.635 = 1.635 mm
+	-> Width => 0.5 + 0.635 = 1.135 mm
+DFA:
+	-> Expand 0.635 mm from the SST
