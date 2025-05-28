6bc1e15d6d69fd0fc910619119feb8e6d13f7893
MUTHU RAMAN S <127648597+muthuraman0508@users.noreply.github.com>
2025-05-28 11:23:25 +0530

    0603_Res_PCB_FootPrint

---
+++ Footprint/Resistor/SMD/0603/dimension.v
@@ -0,0 +1,30 @@
+Resistor: 0603
+Type : SMD
+Units : mm
+
+Pad dimension:
+Shape: Rectangular
+Pad:
+	-> Length => 0.9 mm
+	-> Width => 0.6 mm
+Solder Pad:
+	-> Length => 0.9 mm
+	-> Width => 0.6 mm
+Solder Mask:
+	-> Length => 0.9 + 0.1524 = 1.0524 mm
+	-> Width => 0.6 + 0.1524 = 0.7524 mm
+
+
+Pitch : 0.6 + 0.9 => 1.5 mm
+Assemely Top:
+	-> length => 1.55 mm
+	-> Width => 0.85 mm
+	-> Line width => 0.1270 mm
+Place Bound Top:
+	=> height => 0.45 mm
+
+Sill Screen:
+	-> length => 1.55 + 0.635 = 2.185 mm
+	-> Width => 0.85 + 0.635 = 1.485 mm
+DFA:
+	-> Expand 0.635 mm from the SST
