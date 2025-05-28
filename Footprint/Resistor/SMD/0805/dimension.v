c67ece471fdaac539b9929f09f1142e4341f48b9
MUTHU RAMAN S <127648597+muthuraman0508@users.noreply.github.com>
2025-05-28 11:43:31 +0530

    0805_Res_SMD_FootPrint

---
+++ Footprint/Resistor/SMD/0805/dimension.v
@@ -0,0 +1,30 @@
+Resistor: 0805
+Type : SMD
+Units : mm
+
+Pad dimension:
+Shape: Rectangular
+Pad:
+	-> Length => 1.3 mm
+	-> Width => 0.7 mm
+Solder Pad:
+	-> Length => 1.3 mm
+	-> Width => 0.7 mm
+Solder Mask:
+	-> Length => 1.3 + 0.1524 = 1.4524 mm
+	-> Width => 0.7 + 0.1524 = 0.8524 mm
+
+
+Pitch : 0.7 + 1.2 => 1.9 mm
+Assemely Top:
+	-> length => 2 mm
+	-> Width => 1.2 mm
+	-> Line width => 0.1270 mm
+Place Bound Top:
+	=> height => 0.45 mm
+
+Sill Screen:
+	-> length => 2 + 0.635 = 2.635 mm
+	-> Width => 1.2 + 0.635 = 1.835 mm
+DFA:
+	-> Expand 0.635 mm from the SST
